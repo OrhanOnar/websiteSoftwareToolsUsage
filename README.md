54ee4f48333076fd0ff1da8ec04698df7bb296e0
Orhan <orhanonar@gmail.com>
2018-02-16 21:04:12 +0300

    changing readme and testclass for last options

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[![Build Status](https://travis-ci.org/aweneres/myDemoApp.svg?branch=master)](https://travis-ci.org/aweneres/myDemoApp)
+[![Build Status](https://travis-ci.org/aweneres/myDemoApp.svg?branch=master)](https://travis-ci.org/aweneres/myDemoApp)https://stormy-basin-24796.herokuapp.com/compute
 
-https://stormy-basin-24796.herokuapp.com/compute
+
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -22,7 +22,12 @@ public class AppTest
       ArrayList<Integer> array = new ArrayList<>();
       assertFalse(new App().search(array, 1));
     }
-
+ public void SearchmergedArrayCorrectly() {
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+      ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(5, 6, 7, 8));
+        array.addAll(array2);
+      assertFalse(new App().search(array, 7));
+    }
     public void testNull() {
       assertFalse(new App().search(null, 1));
     }
